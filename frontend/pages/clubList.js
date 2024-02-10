f0b5b4b3802c1b7c419332340b18b9055239151f
FelixGustafsson <felixgustafssong@gmail.com>
2024-02-10 11:43:35 +0100

    add clublist page to router

---
+++ frontend/pages/clubList.js
@@ -0,0 +1,7 @@
+export default async function clubList() {
+  return `
+    <div id="main-club-list-container">
+        <h1>OUR CLUBS</h1>
+    </div>
+    `;
+}
--- frontend/router.js
+++ frontend/router.js
@@ -1,12 +1,15 @@
-import home from "./pages/home.js";
+import home from './pages/home.js';
+import clubList from './pages/clubList.js';
 
 async function route() {
-  switch (location.hash.replace("#", "")) {
-    case "":
-      $("main").html(await home());
+  switch (location.hash.replace('#', '')) {
+    case '':
+      $('main').html(await home());
       break;
+    case 'clubs':
+      $('main').html(await clubList());
     default:
-      console.log("404 Page Not found");
+      console.log('404 Page Not found');
   }
 }
 
