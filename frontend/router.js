0d4760fc7461c0ab01cfb7012fa3963034acd0f1
FelixGustafsson <felixgustafssong@gmail.com>
2024-02-10 11:26:23 +0100

    add login page to the frontend router

--- frontend/router.js
+++ frontend/router.js
@@ -1,12 +1,17 @@
-import home from "./pages/home.js";
+import home from './pages/home.js';
+import login from './pages/login.js';
 
 async function route() {
-  switch (location.hash.replace("#", "")) {
-    case "":
-      $("main").html(await home());
+  switch (location.hash.replace('#', '')) {
+    case '':
+      $('main').html(await home());
+      break;
+    case 'login':
+      console.log('hej');
+      $('main').html(login());
       break;
     default:
-      console.log("404 Page Not found");
+      console.log('404 Page Not found');
   }
 }
 
