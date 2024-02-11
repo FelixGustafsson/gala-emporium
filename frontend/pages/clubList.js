b08fc864b9d9717caf6836ec21d14adc48e3c5e1
FelixGustafsson <felixgustafssong@gmail.com>
2024-02-11 15:48:50 +0100

    add the clubs on the page and add styling

--- frontend/pages/clubList.js
+++ frontend/pages/clubList.js
@@ -4,9 +4,28 @@ export default async function clubList() {
 
   console.log(result);
 
+  let html = '';
+
+  for (let data of result) {
+    html += `
+        <div id="club-container">
+          <div>
+            <h1>${data.name}</h1>
+            <br/>
+            <p>${data.description}</p>
+            <br/>
+            <button>Read more</button>
+          </div>
+          <img src="${data.imageURL}" alt="${data.name} picture"></img>
+        </div>
+      `;
+  }
+
   return `
     <div id="main-club-list-container">
         <h1>OUR CLUBS</h1>
+        <br/>
+        <div>${html}</div>
     </div>
     `;
 }
