4d601404e04c67a9268a952cceb9dfe8e3deaaf9
DamirCoco <143598880+DamirCoco@users.noreply.github.com>
2024-02-12 11:48:10 +0100

    fix error

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -5,8 +5,8 @@ export default async function home() {
      <h1>About us</h1>
     </div>
       <div id="about-text">
-          <p>We at Gala Emporium help clubs market there events and hoste there events in our venue.
-          We have been in the business for over 20 years, and we strive to hoste the best events and make our clients happy.
+          <p>We at Gala Emporium help clubs market there events and host there events in our venue.
+          We have been in the business for over 20 years, and we strive to host the best events and make our clients happy.
           We sit down with our clients and plan the events so that you as a client feel that you have controul of every step of 
           the event planing. </p>
       </div>
