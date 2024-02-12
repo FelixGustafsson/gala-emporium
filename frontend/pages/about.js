a857eb6fb93a24d3f819c39aad90ecf896bf7442
DamirCoco <143598880+DamirCoco@users.noreply.github.com>
2024-02-12 11:44:39 +0100

    add about text an styling

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,14 +1,17 @@
 export default async function home() {
   return `
   <div id="main-about-container">
+    <div id = "title">
+     <h1>About us</h1>
+    </div>
       <div id="about-text">
-          <h1>We at Gala Emporium help clubs market there events and hoste there events in our venue.
+          <p>We at Gala Emporium help clubs market there events and hoste there events in our venue.
           We have been in the business for over 20 years, and we strive to hoste the best events and make our clients happy.
           We sit down with our clients and plan the events so that you as a client feel that you have controul of every step of 
-          the event planing. </h1>
+          the event planing. </p>
       </div>
       <div id ="assets">
-        <img src = "../assets/aboutpraties.jpeg" alt="our events">
+        <img src = "../assets/aboutparties.jpeg" alt="our events">
       </div>
   </div>
   `;
