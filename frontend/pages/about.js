52012145c9a95a4b23b5e976c7b875fb0edd60f0
DamirCoco <143598880+DamirCoco@users.noreply.github.com>
2024-02-12 10:50:41 +0100

    add about text and image to about page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -2,7 +2,13 @@ export default async function home() {
   return `
   <div id="main-about-container">
       <div id="about-text">
-          <h1>ABOUT GALA EMPORIUM</h1>
+          <h1>We at Gala Emporium help clubs market there events and hoste there events in our venue.
+          We have been in the business for over 20 years, and we strive to hoste the best events and make our clients happy.
+          We sit down with our clients and plan the events so that you as a client feel that you have controul of every step of 
+          the event planing. </h1>
+      </div>
+      <div id ="assets">
+        <img src = "../assets/aboutpraties.jpeg" alt="our events">
       </div>
   </div>
   `;
