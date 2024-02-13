b7bae001e270573bf556beab2086a7e041ac7da7
FelixGustafsson <felixgustafssong@gmail.com>
2024-02-13 09:25:14 +0100

    improve styling on book events page

--- frontend/pages/bookEvents.js
+++ frontend/pages/bookEvents.js
@@ -1,28 +1,27 @@
 export default async function bookEvents() {
-    const response = await fetch("http://localhost:3000/api/event");
-    const result = await response.json();
-    let html = "";
+  const response = await fetch('http://localhost:3000/api/event');
+  const result = await response.json();
+  let html = '';
 
-    for (let data of result) {
-        html += `
-        <div id="event-container">
+  for (let data of result) {
+    html += ` 
           <li>
-            <h1>${data.name}</h1>
-            <p>${data.description}</p>
-            <button>Book ${data.name}</button>
+            <div>
+              <h2>${data.name}</h2>
+              <p>${data.description}</p>
+              <button>Book ${data.name}</button>
+            </div>
             <img src="${data.imageURL}" alt="Event image"></img>
-        
           </li>
-        </div>
         `;
-        }
+  }
 
-    return `
-    <div id="main-container">
-    <div id="events-container">
-    <h1>Book Events</h1>
-    <ul>${html}</ul>
-    </div>
-  </div>
+  return `
+      <div id="main-container">
+        <div id="events-container">
+          <h1>Book Events</h1>
+          <ul>${html}</ul>
+        </div>
+      </div>
     `;
 }
