5d6c384a328da9eb716ed066299d1fc1263ec382
Felix Gustafsson <felixgustafssong@gmail.com>
2024-02-19 14:01:26 +0100

    fix bug with clublist page

--- frontend/pages/clubList.js
+++ frontend/pages/clubList.js
@@ -1,5 +1,5 @@
 export default async function clubList() {
-  const response = await fetch('http://localhost:3000/api/clubs');
+  const response = await fetch('http://localhost:3000/api/club');
   const result = await response.json();
 
   console.log(result);
