3961aa19f77755922cd6d416bad991a17b0bc0dc
DamirCoco <143598880+DamirCoco@users.noreply.github.com>
2024-02-19 14:28:13 +0100

    fixt text

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -3,10 +3,10 @@ export default async function home() {
   <div id="main-about-container">
     <div id="about-text">
       <h1>About us</h1>
-      <p>We at Gala Emporium help clubs market there events and host there events in our venue.
+      <p>We at Gala Emporium help clubs market their events and host their events in our venue.
         We have been in the business for over 20 years, and we strive to host the best events and make our clients happy.
-        We sit down with our clients and plan the events so that you as a client feel that you have controul of every step of 
-        the event planing. 
+        We sit down with our clients and plan the events so that you as a client feel that you have control of every step of 
+        the event planning. 
       </p>
     </div>
     <img src = "../assets/aboutparties.jpeg" alt="our events">
