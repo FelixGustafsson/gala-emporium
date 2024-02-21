704a3c86fac18e54bf04f13646636176ce510e00
Felix Gustafsson <felixgustafssong@gmail.com>
2024-02-21 11:00:18 +0100

    comment code on profile page

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,4 +1,5 @@
 export default async function profile() {
+  //Fetching the logged in users information
   const userResponse = await fetch('/api/login');
   const user = await userResponse.json();
   const profileUserResponse = await fetch(`/api/user/${user.login}`);
@@ -6,13 +7,16 @@ export default async function profile() {
 
   let html = '';
 
+  //Checks if the logged in user is a club owner
   if (profileUser.isClubOwner) {
+    //Fetches all the events on the page
     const eventsResponse = await fetch('/api/event');
     const events = await eventsResponse.json();
 
+    //Sorts the events so that it only shows the events for their club
     for (let data of events) {
       if (data.club === profileUser.club) {
-        //add delete method for the event both in backend and onCick at the button
+        //Generates an event <li> for every event created by the club owner
         html += `
         <li>
           <h2>${data.name}</h2>
@@ -23,6 +27,7 @@ export default async function profile() {
       `;
       }
     }
+    //Builds the html structure for the profile page of a club owner
     return `
     <div id="main-profile-container">
       <div>
@@ -70,10 +75,13 @@ export default async function profile() {
     </div>
         `;
   } else {
+    //Profile page code for a normal user
     for (let data of profileUser.bookedEvents) {
+      //fetches all the events that's in the profileUser.bookedEvents array
       const eventResponse = await fetch(`/api/event/${data.event[0]}`);
       const event = await eventResponse.json();
 
+      //Generates an event <li> for every event booked by the user
       html += `
         <li>
             <h3>${event.name} - ${data.numberOfTickets} Tickets</h3>
@@ -81,6 +89,7 @@ export default async function profile() {
         </li>
     `;
     }
+    //The main html structure for a normal user profile page
     return `
     <div id="main-profile-container-privateperson">
         <h1>Welcome ${profileUser.name}</h1>
@@ -95,6 +104,7 @@ export default async function profile() {
   }
 }
 
+//Function for cancelling a booking as a normal user 
 async function cancelBooking(id) {
   let response = await fetch(`/api/booking/${id}`, {
     method: 'delete',
@@ -104,6 +114,7 @@ async function cancelBooking(id) {
   location.reload();
 }
 
+//Function for deleting an event as a club owner
 async function deleteEvent(id) {
   let response = await fetch(`/api/event/${id}`, {
     method: 'delete',
@@ -111,10 +122,10 @@ async function deleteEvent(id) {
   location.reload();
 }
 
+//Function for changing the color theme of the club page as a club owner
 async function updateColorTheme(id) {
   const theme = $('[name=colorTheme]:checked').val();
 
-  console.log(theme);
   let response = await fetch(`/api/club/${id}`, {
     method: 'PATCH',
     headers: { 'Content-Type': 'application/json' },
@@ -124,7 +135,9 @@ async function updateColorTheme(id) {
   console.log(response);
 }
 
+//function for creating a new event as a club owner
 async function createNewEvent(clubID) {
+  //THe event object that gets sent to the server
   let newEvent = {
     name: $('[name=event-name]').val(),
     description: $('[name=description]').val(),
@@ -141,6 +154,8 @@ async function createNewEvent(clubID) {
     body: JSON.stringify(newEvent),
   });
   console.log(response);
+
+  //Show different text depending on if the event creation was succesful or not
   $('#event-form')[0].reset();
   if (response.status == 200) {
     $('#registration-text').text('New event created!');
