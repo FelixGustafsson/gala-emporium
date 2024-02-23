9f2abdd9cb0963c41d3ac64e45f431154b477250
FelixGustafsson <felixgustafssong@gmail.com>
2024-02-23 08:25:27 +0100

    add comments to booking.js

--- backend/api/booking.js
+++ backend/api/booking.js
@@ -1,36 +1,41 @@
-import bookingModel from "../models/bookingModel.js";
-import eventModel from "../models/eventModel.js";
-import userModel from "../models/userModel.js";
+import bookingModel from '../models/bookingModel.js';
+import eventModel from '../models/eventModel.js';
+import userModel from '../models/userModel.js';
 
 export default function club(server) {
-  server.post("/api/booking/:id", async (req, res) => {
+  //posting a new booking
+  server.post('/api/booking/:id', async (req, res) => {
+    //Finding the event tthat the user wants to book
     const event = await eventModel.findById(req.params.id);
+    //Checks to see if there's enough tickets left
     if (event.tickets >= req.body.numberOfTickets) {
+      //If there's enough tickets we create a new booking object
       const booking = new bookingModel({
         user: req.body.user,
         event: req.body.event,
         numberOfTickets: req.body.numberOfTickets,
       });
+      //Send the new booking to the database and decrease available tickets from the event
       const result = await booking.save();
       event.tickets -= req.body.numberOfTickets;
       event.save();
+      //Add the booking to the user
       const user = await userModel.findById(req.body.user);
       user.bookedEvents.push(booking);
       user.save();
       res.json(result);
     } else {
-      res.json({ message: "Out of tickets" });
+      res.json({ message: 'Out of tickets' });
     }
   });
 
-  server.delete("/api/booking/:id", async (req, res) => {
-    //finds the booking and the user
-    console.log("request id " + req.params.id);
+  //Deletes a booking
+  server.delete('/api/booking/:id', async (req, res) => {
+    //Finds the booking and the user
     const booking = await bookingModel.findById(req.params.id);
     const event = await eventModel.findById(booking.event[0]);
     const user = await userModel.findById(booking.user[0]);
-    //at the moment, there is no code to remove the booking from the user's bookedEvents array
-    // code needed here
+
     let array = user.bookedEvents;
     for (let i = array.length - 1; i >= 0; i--) {
       if (array[i]._id == req.params.id) {
